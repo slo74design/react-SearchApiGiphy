a43c7da22d7a38d38363ab56bc4219044f6fcaa3
slo74design <slo74design@gmail.com>
2022-01-05 19:15:46 +0200

    Api url  actualizado - 5 Jan 2022

--- src/helpers/getGiphy.js
+++ src/helpers/getGiphy.js
@@ -3,7 +3,7 @@
     export const getGiphys = async ( category ) => { 
         const apiKey = '74RjVwZ6O3uBxklYrNq8uynypnygO14H'; // registrado como byuppy en GIPHY
         // URL Giphy with ApiKey: http://api.giphy.com/v1/gifs/random?api_key=74RjVwZ6O3uBxklYrNq8uynypnygO14H
-        const url = await fetch(`http://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=${ apiKey }`);
+        const url = await fetch(`https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=${ apiKey }`);
         // desestructuro el primer nodo del API que se llama data
         const { data } = await url.json(); 
 
